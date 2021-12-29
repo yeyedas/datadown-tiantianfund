c1e75ed2b83858eefaceb48c4d0974b0fa7816de
yeyedas <digerev@126.com>
2021-12-29 23:25:42 +0800

    update new selection

--- fund_earn_var_cal.py
+++ fund_earn_var_cal.py
@@ -12,10 +12,10 @@ cal hybrid_fund_data to hybrid_earn_var
 import pandas as pd
 from sqlalchemy import create_engine
 import tqdm
+import click
 
 def cal_day_earn_and_var(fund_list, date_latest, date_end, name, update, engine):
-    cal_day_earn = [30, 90, 180, 270, 365]
-    cal_day_var = [90, 365]
+    cal_day = [7, 30, 90, 180, 270, 365]
 
     for fund in tqdm.tqdm(fund_list.fund):
 
@@ -34,29 +34,47 @@ def cal_day_earn_and_var(fund_list, date_latest, date_end, name, update, engine)
             drop=True)
         fund_data = fund_data.fillna(method='ffill')
 
-        df = pd.DataFrame()
-        df.loc[:, 'date'] = fund_data.date
-        df.loc[:, 'fund'] = fund
-        for day in cal_day_earn:
+        earn_df = pd.DataFrame()
+        earn_df.loc[:, 'date'] = fund_data.date
+        earn_df.loc[:, 'fund'] = fund
+        var_df = pd.DataFrame()
+        var_df.loc[:, 'date'] = fund_data.date
+        var_df.loc[:, 'fund'] = fund
+        for day in cal_day:
             days_grand_total = (fund_data.ACWorth - fund_data.ACWorth.shift(day)) / fund_data.ACWorth.shift(day)
-            df.loc[:, '%ddays_earn' % (day)] = days_grand_total
-        for day in cal_day_var:
-            df.loc[:, '%ddays_var' % (day)] = fund_data.ACWorth.rolling(day + 1).var()
+            earn_df.loc[:, '%ddays_earn' % (day)] = days_grand_total
+        for day in cal_day:
+            var_df.loc[:, '%ddays_var' % (day)] = fund_data.ACWorth.rolling(day + 1).var()
 
-        df.loc[:, 'second_increase'] = df.loc[:, '90days_earn'].shift(90)
-        df.loc[:, 'third_increase'] = df.loc[:, 'second_increase'].shift(90)
-        df.loc[:, 'fourth_increase'] = df.loc[:, 'third_increase'].shift(90)
+        earn_df.loc[:, 'second_increase'] = earn_df.loc[:, '90days_earn'].shift(90)
+        earn_df.loc[:, 'third_increase'] = earn_df.loc[:, 'second_increase'].shift(90)
+        earn_df.loc[:, 'fourth_increase'] = earn_df.loc[:, 'third_increase'].shift(90)
 
-        df = df.merge(date_list, how='right').dropna()
-        df = df[df.date > date_end]
+        var_df.loc[:, 'second_increase'] = var_df.loc[:, '90days_var'].shift(90)
+        var_df.loc[:, 'third_increase'] = var_df.loc[:, 'second_increase'].shift(90)
+        var_df.loc[:, 'fourth_increase'] = var_df.loc[:, 'third_increase'].shift(90)
 
-        df = df[['fund', 'date', '30days_earn', '90days_earn', '180days_earn', '270days_earn', '365days_earn',
-                 'second_increase', 'third_increase', 'fourth_increase', '90days_var', '365days_var']]
-        if df.empty == False:
+        earn_df = earn_df.merge(date_list, how='right').dropna()
+        earn_df = earn_df[earn_df.date > date_end]
+        var_df = var_df.merge(date_list, how='right').dropna()
+        var_df = var_df[var_df.date > date_end]
+
+        earn_df = earn_df[
+            ['fund', 'date', '7days_earn', '30days_earn', '90days_earn', '180days_earn', '270days_earn', '365days_earn',
+             'second_increase', 'third_increase', 'fourth_increase']]
+        var_df = var_df[
+            ['fund', 'date', '7days_var', '30days_var', '90days_var', '180days_var', '270days_var', '365days_var',
+             'second_increase', 'third_increase', 'fourth_increase']]
+        if earn_df.empty == False:
             if (update == False) & (fund == fund_list.loc[0].values):
-                df.to_sql(name='%s_earn_var' % (name), con=engine, index=False, if_exists='replace')
+                earn_df.to_sql(name='%s_earn' % (name), con=engine, index=False, if_exists='replace')
             else:
-                df.to_sql(name='%s_earn_var' % (name), con=engine, index=False, if_exists='append')
+                earn_df.to_sql(name='%s_earn' % (name), con=engine, index=False, if_exists='append')
+        if var_df.empty == False:
+            if (update == False) & (fund == fund_list.loc[0].values):
+                var_df.to_sql(name='%s_var' % (name), con=engine, index=False, if_exists='replace')
+            else:
+                var_df.to_sql(name='%s_var' % (name), con=engine, index=False, if_exists='append')
 
 @click.command()
 @click.option('--account', default='root', help='Account of mysql')
@@ -69,26 +87,27 @@ def cal_day_earn_and_var(fund_list, date_latest, date_end, name, update, engine)
 def main_command(account, password, host, post, database, update):
 
     engine = create_engine('mysql+pymysql://%s:%s@%s:%s/%s' % (account, password, host, post, database))
-    name = 'hybrid'
+    names = ['hybrid', 'bond', 'equity', 'index', 'qdii', 'commodity']
 
-    date_latest = pd.read_sql('SELECT MAX(date) FROM %s_fund_data' % (name), engine).loc[0, 'MAX(date)']
-    fund_list = pd.read_sql('SELECT DISTINCT fund FROM %s_fund_data WHERE date=\'%s\'' % (name, date_latest), engine)\
-        .sort_values(by='fund')
+    for name in names:
+        date_latest = pd.read_sql('SELECT MAX(date) FROM %s_fund_data' % (name), engine).loc[0, 'MAX(date)']
+        fund_list = pd.read_sql('SELECT DISTINCT fund FROM %s_fund_data WHERE date=\'%s\'' % (name, date_latest), engine)\
+            .sort_values(by='fund')
 
-    if update:
-        date_end = \
-        pd.read_sql('SELECT MAX(date) FROM %s_earn_var WHERE fund=\'%s\'' % (name, fund_list.iloc[0, 0]),
-                    engine).iloc[0, 0]  # 当前数据最后日期
-    else:
-        date_end = '2015-01-01'
-    cal_day_earn_and_var(fund_list, date_latest, date_end, name, update, engine)
+        if update:
+            date_end = \
+            pd.read_sql('SELECT MAX(date) FROM %s_earn_var WHERE fund=\'%s\'' % (name, fund_list.iloc[0, 0]),
+                        engine).iloc[0, 0]  # 当前数据最后日期
+        else:
+            date_end = '2015-01-01'
+        cal_day_earn_and_var(fund_list, date_latest, date_end, name, update, engine)
 
-    if update == False:
-        with engine.connect() as con:
-            con.execute('create index s1 on %s_earn_var(fund(6))' % (name))  # 创建索引
-            con.execute('create index s2 on %s_earn_var(date(12))' % (name))  # 创建索引
-            con.execute('create index s3 on %s_earn_var(fund(6),date(12))' % (name))  # 创建索引
-            con.close()
+        if update == False:
+            with engine.connect() as con:
+                con.execute('create index s1 on %s_earn_var(fund(6))' % (name))  # 创建索引
+                con.execute('create index s2 on %s_earn_var(date(12))' % (name))  # 创建索引
+                con.execute('create index s3 on %s_earn_var(fund(6),date(12))' % (name))  # 创建索引
+                con.close()
 
 
 if __name__ == '__main__':
